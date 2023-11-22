08d29f7f452bdaa0116c3a056c43bf42befe2bcd
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-22 13:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function activateSearch() {
+    const searchDiv = document.querySelector('.search');
+    const input = document.querySelector('.input');
+
+    // Toggle the 'active' class on the search div
+    searchDiv.classList.toggle('active');
+
+    // Focus on the input after clicking the button
+    if (searchDiv.classList.contains('active')) {
+      input.focus();
+    }
+  }
